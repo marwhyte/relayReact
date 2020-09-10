e5f34ca22c4418de3935b2998de0e6bece6977dd
Marco Whyte <mwhyte@umass.edu>
2020-09-10 13:15:19 -0500

    changed to javascript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>hi</p>
+    </div>
+  );
 }
 
 export default App;
