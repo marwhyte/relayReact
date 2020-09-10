5a2d2195cf3553a8af68c25714b3e7b46690ec74
Marco Whyte <mwhyte@umass.edu>
2020-09-10 13:29:25 -0500

    relay config

---
+++ relay.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //used to configure what is allowed in relay compiler
+  src: "./src",
+  schema: "./data/schema.graphql",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+};
